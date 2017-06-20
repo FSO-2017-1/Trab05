ff73cdb899223a0c1f13df8f33cb3f53998eda1d
joaohenriquepda <joaohenrique.p.almeida@gmail.com>
2017-06-20 00:10:45 -0300

    final commit for seek in file

--- main.c
+++ main.c
@@ -5,62 +5,56 @@
 
 int contador_resultados = 0;
 
-void ler_arquivo(char* local){
+void ler_arquivo(char* local, char * arquivo, int max_resultados){
+	if (contador_resultados == max_resultados) return;
+	if (!strstr(local, arquivo)) return;
 
-
-	FILE *arquivo_alvo = fopen(local, "r");
-	char bytes[30];
-
-	//imprime só primeira linha
-	if(arquivo_alvo == NULL){
-		perror("Erro ao abrir arquivo");
-	}
-
-	fgets(bytes, 30, arquivo_alvo);
-	printf("%s", bytes);
-
-
-	fclose(arquivo_alvo);
+	FILE* sigD = fopen(local, "r");
+	char sig[30];
+	fread(sig, 1, 30, sigD);
+	contador_resultados++;
+	printf("%d. %s --\n", contador_resultados, local);
+	printf("\t%s\n",sig);
 }
 
 void pesquisar_diretorio(char* local, char * arquivo, int max_resultados){
+
 	DIR *diretorio;
-    struct dirent *dir;
-    diretorio = opendir(local);
+  struct dirent *dir;
+  diretorio = opendir(local);
 
 	if (diretorio){
-		while ((dir = readdir(diretorio)) != NULL) {
+		while ((dir = readdir(diretorio)) != NULL){
 			char nome_local[256] = "";
-	        strcat(nome_local, local);
+			strcat(nome_local, local);
 			strcat(nome_local, dir->d_name);
 
-			if(!(strcmp(dir->d_name, ".") == 0) && !(strcmp(dir->d_name, "..") == 0)){
-				if(dir->d_type == DT_DIR){
-					strcat(nome_local, "/");
-					pesquisar_diretorio(nome_local, arquivo, max_resultados);
-				}else if(strncmp(dir->d_name, arquivo, 4) == 0){
-//compara a substring de até 4 caracteres, não sei se é o melhor jeito
-					if(contador_resultados >= max_resultados){
-						return;
+			if(!(strcmp(dir->d_name, ".") == 0)){
+				if(!(strcmp(dir->d_name, "..") == 0)){
+					if(dir->d_type == 4){
+						strcat(nome_local, "/");
+						pesquisar_diretorio(nome_local, arquivo, max_resultados);
 					}
-					printf("%d. %s --\n", contador_resultados+1, nome_local);
-					contador_resultados++;
-					ler_arquivo(nome_local);
-					printf("\n");
-
+					else{
+						ler_arquivo(nome_local, arquivo, max_resultados);
+					}
 				}
 			}
-//closedir(diretorio);
 		}
-
+		closedir(diretorio);
 	}
-
 }
 
 
 int main(int argc, char *argv[]){
 	int max_resultados = atoi(argv[3]);
-	printf("Resultado do buscador -- '%s' na pasta %s\n", argv[2], argv[1]);
+
+	if(argc != 4){
+			 printf("Use: ./buscador /caminho/que/deseja palavra_de_busca numero_max_de_resultados\n");
+			 exit(EXIT_FAILURE);
+	}
+
+	printf("\n \nResultado do buscador -- '%s' na pasta %s\n", argv[2], argv[1]);
 	pesquisar_diretorio(argv[1], argv[2], max_resultados);
 
 	return(0);
